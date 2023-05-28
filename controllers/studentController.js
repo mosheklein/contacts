f4ad69e312b4063f7dd4ab5d279efb05f381a678
Moshe Klienblut <moshek@college.labs>
2023-05-28 14:53:01 +0300

    MVC Finished

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,5 +1,5 @@
 // student_index, student_details, student_create_get, student_create_post, student_delete
-
+// student_update
  const student_index = (req, res) => {
     student.findAll().then( students => res.render('students',{students}))
 }
@@ -13,8 +13,38 @@ const student_details = (req, res) => {
     }).then( data => res.send(data))
 }
 
+
+const student_create_get =  (req, res) => {
+    res.render('new-student')
+}
+
+const student_create_post = (req, res) => {
+    const newStudent = req.body
+    student.create(newStudent).catch( e => console.log(e))
+     res.send(newStudent)
+ }
+
+
+const student_delete =  (req, res) => {
+    const studentID = req.params.id
+    student.destroy({
+        where:{id:studentID}
+    })
+
+    res.send({location:'/students'})
+}
+
+const student_update =  (req, res) => {
+    const studentID = req.params.id // 1
+    student.update({email:'s@gmail.com'},{where:{id:studentID}}).then( data => res.send(data))
+}
+
 module.exports = {
     student_index,
-    student_details
+    student_details,
+    student_create_get,
+    student_create_post,
+    student_delete,
+    student_update
 }
 
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -5,53 +5,9 @@ const studentController = require('./../controllers/studentController')
 
 router.get('/', studentController.student_index)
 router.get('/:id', studentController.student_details)
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.get('/create', (req, res) => {
-    res.render('new-student')
-})
-
-router.post('/', (req, res) => {
-   const newStudent = req.body
-   student.create(newStudent).catch( e => console.log(e))
-    res.send(newStudent)
-})
-
-router.delete('/', (req, res) => {
-    student.destroy({
-        where:{},
-        truncate:true
-    })
-
-    res.send('All Records have been deleted')
-})
-
-router.delete('/:id', (req, res) => {
-    const studentID = req.params.id
-    student.destroy({
-        where:{id:studentID}
-    })
-
-    res.send({location:'/students'})
-})
-
-
-
-
-router.put('/:id', (req, res) => {
-    const studentID = req.params.id // 1
-    student.update({email:'s@gmail.com'},{where:{id:studentID}}).then( data => res.send(data))
-})
+router.get('/create', studentController.student_create_get)
+router.post('/', studentController.student_create_post)
+router.delete('/:id', studentController.student_delete)
+router.put('/:id', studentController.student_update)
 
 module.exports = router
